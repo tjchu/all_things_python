636625134a70b35b3fb5ab6125183718a069b22d
Terry Chu <terrychu2002@gmail.com>
2018-11-18 17:00:44 -0800

    Added comments to clarify the code

--- flask/flaskr/__init__.py
+++ flask/flaskr/__init__.py
@@ -1,4 +1,4 @@
-/usr/local/bin/python3
+#!/usr/local/bin/python3
 
 import os
 
@@ -6,13 +6,33 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    # create and configure the app
+    """
+    create and configure the app, creates the Flask instance
+
+    instance_relative_config=True tells the app that configuration files are relative to the instance folder.
+    The instance folder is located outside the flaskr package and can hold local data that shouldn’t be committed to
+    version control, such as configuration secrets and the database file.
+    """
     app = Flask(__name__, instance_relative_config=True)
+
+    """
+    app.config.from_mapping() sets some default configuration that the app will use:
+    
+    SECRET_KEY is used by Flask and extensions to keep data safe. It’s set to 'dev' to provide a convenient value 
+    during development, but it should be overridden with a random value when deploying.
+    
+    DATABASE is the path where the SQLite database file will be saved. It’s under app.instance_path, 
+    which is the path that Flask has chosen for the instance folder.
+    """
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
+    """
+    overrides the default configuration with values taken from the config.py file in the instance folder if it exists. 
+    For example, when deploying, this can be used to set a real SECRET_KEY.
+    """
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
