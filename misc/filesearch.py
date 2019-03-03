8b0b9b022d05c2a6f8fda83aad26237c231a5239
Terry Chu <terrychu2002@gmail.com>
2019-03-02 18:37:35 -0800

    Updates on filesearch.py

--- misc/filesearch.py
+++ misc/filesearch.py
@@ -2,16 +2,24 @@ import os
 import argparse
 
 #Parameters to pass into this script
-parser = argparse.ArgumentParser(description="Inputs needed to run this shit")
+parser = argparse.ArgumentParser(description="Inputs needed to run the file searching Python program")
 parser.add_argument('--file', '-f', action='store', dest='file', help="File name to find", required=True)
 parser.add_argument('--dir', '-d', action='store', dest='dir', help='Directory to search', required=True)
 
 args = parser.parse_args()
 
+founded_files = []
 
 for root, dirs, files in os.walk(args.dir):
   for file in files:
   	if file == args.file:
-  		print("FOUND:", os.path.join(root, file))
+  		founded_files.append(os.path.join(root, file))
 
+if len(founded_files) >= 1:
+	print(founded_files)
+	exit()
+
+  		
+
+print("No file(s) %s was found" % args.file)
 
