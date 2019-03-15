8e68a58994ae1f2b3d319874a78612201373726c
Terry Chu <terrychu2002@gmail.com>
2019-03-14 21:18:35 -0700

    Added a phrase search Python script that is able to find all of the files that contain a string and output the line number

---
+++ misc/phrasesearch.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+
+#Parameters to pass into this script
+parser = argparse.ArgumentParser(description="Inputs needed to run the phrase searching Python program")
+parser.add_argument('--phrase', '-p', action='store', dest='phrase', help="Phrase to find", required=True)
+parser.add_argument('--dir', '-d', action='store', dest='dir', help='Directory to search', required=True)
+args = parser.parse_args()
+
+print("Welcome to Terry's Phrase Search Program")
+print("This program will be able to recursively find a phrase in a file or a directory") 
+
+
+for root, directories, files in os.walk(args.dir):
+	for file in files:
+		with open(file) as f:
+			for linenumber, line in enumerate(f, 1):
+				if args.phrase in line:
+					print("The '%s' is in file '%s' on line %d" % (args.phrase, file, linenumber))
+		
+
+print("Phrase searching done.")
+			
+
+
+
+"""
+with open(filename) as myFile:
+    for num, line in enumerate(myFile, 1):
+        if lookup in line:
+            print 'found at line:', num
+"""
