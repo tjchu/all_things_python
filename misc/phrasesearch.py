e5fe1259c8ffc7a9e76b7721d76a04967200cd78
Terry Chu <terrychu2002@gmail.com>
2019-03-14 21:22:20 -0700

    Inserted comments explaining what the code is doing

--- misc/phrasesearch.py
+++ misc/phrasesearch.py
@@ -11,9 +11,15 @@ print("Welcome to Terry's Phrase Search Program")
 print("This program will be able to recursively find a phrase in a file or a directory") 
 
 
-for root, directories, files in os.walk(args.dir):
+# os.walk() can traverse through all of the subdirectories and list all of the files and directories
+# in each directory it traverses through. 
+# It returns a tuple (current_directory, sub-directories in the current dir, files in current dir)
+for _, _, files in os.walk(args.dir):
 	for file in files:
 		with open(file) as f:
+
+			# Use enumerate() - adds counter to an iterable and returns it. 
+			# The returned object is a enumerate object.
 			for linenumber, line in enumerate(f, 1):
 				if args.phrase in line:
 					print("The '%s' is in file '%s' on line %d" % (args.phrase, file, linenumber))
