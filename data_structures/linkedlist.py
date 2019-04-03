d266e9e257974273e65fd8904542eedc32d222d7
Terry Chu <terrychu2002@gmail.com>
2019-04-03 14:55:34 -0700

    Updates on LinkedList

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -1,5 +1,5 @@
 # Implementing a singly Linked List
-class Node(object):
+class ListNode(object):
 
 	def __init__(self, data=None, next_node=None):
 		self.data = data
@@ -14,18 +14,21 @@ class Node(object):
 	def set_next(self, new_next):
 		self.next = new_next
 
-	# Search for the Linked List node containing a specific data value and return the node
-	# Time Complexity: O(n)
-	def search(self, data):
-		current = self
-		found = False
-		while current != None and found == False:
-			if current.get_data() == data:
-				found = True
-			else:
-				current = current.get_next()
-		if current is None:
-			raise ValueError("Data not in list!")
-		return current
+# Search for the Linked List node containing a specific data value and return the node
+# Time Complexity: O(n)
+def search(node: ListNode, data):
+	current = node
+	found = False
+	while current != None and found == False:
+		if current.get_data() == data:
+			found = True
+		else:
+			current = current.get_next()
+	if current is None:
+		raise ValueError("Data not in list!")
+	return current
 
-	
+
+
+
+#def addTwoNumbers(self, l1: ListNode, l2: ListNode)
