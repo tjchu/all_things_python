0b6dbe1c0f20c2b8ae167fb82708c9091033e0db
Terry Chu <terrychu2002@gmail.com>
2019-04-03 14:45:40 -0700

    Search function for LinkedList

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -14,3 +14,14 @@ class Node(object):
 	def set_next(self, new_next):
 		self.next_node = new_next
 
+	def search(self, data):
+		current = self.head
+		found = False
+		while current and found is False:
+			if current.get_data() == data:
+				found = True
+			else:
+				current = current.get_next()
+		if current is None:
+			raise ValueError("Data not in list")
+		return current
